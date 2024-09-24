b387f6d2793d7ba40ed17182408632af74b9ffa4
Nikita U <neavinstasi@icloud.com>
2024-09-24 18:17:41 +0300

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: path.resolve(__dirname, 'assets'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@ui': path.resolve(__dirname, './src/shared/components/ui'),
+      '@icons': path.resolve(__dirname, './assets/icons'),
+      '@images': path.resolve(__dirname, './assets/images'),
+      '@components': path.resolve(__dirname, './src/shared/components'),
+      '@constants': path.resolve(__dirname, './src/shared/constants'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@hooks': path.resolve(__dirname, './src/shared/hooks'),
+      '@api': path.resolve(__dirname, './src/shared/api'),
+      '@store': path.resolve(__dirname, './src/shared/store'),
+    },
+  },
 });
