54ff4ab395261c40589f68cd39c20b9dda26deb0
Nikita U <neavinstasi@icloud.com>
2024-09-26 22:13:49 +0300

    set up main app files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,14 @@
+import { Suspense } from 'react';
+
+import { Layout } from './layout/Layout';
+import { LayoutLoader } from './layout/LayoutLoader';
+
 export const App = () => {
-  return <div className='App' />;
+  return (
+    <div id='app' className='app'>
+      <Suspense fallback={<LayoutLoader />}>
+        <Layout />
+      </Suspense>
+    </div>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,32 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { getUsersSession } from '@api/requests/users/session/index.ts';
 
 import { App } from './app/App.tsx';
+import { TanStackQueryProvider } from './app/providers/TanStackQueryProvider';
 
-createRoot(document.getElementById('root')!).render(<App />);
+import '@/app/styles/index.css';
+const init = async () => {
+  const token = localStorage.getItem('AUTH_TOKEN');
+
+  if (token) {
+    const getUsersSessionResponse = await getUsersSession();
+
+    const user = { isLoggedIn: true, ...getUsersSessionResponse.data.user };
+
+    console.log(user);
+  }
+
+  createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <TanStackQueryProvider>
+          <App />
+        </TanStackQueryProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
+init();
