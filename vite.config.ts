2af61571260ab02e5a4fdeca7a9af592748656a6
Nikita U <neavinstasi@icloud.com>
2024-09-26 21:22:46 +0300

    add vite svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,37 @@
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        exportType: 'named',
+        namedExport: 'ReactComponent',
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+              active: false,
+            },
+          ],
+        },
+      },
+      include: '**/*.svg',
+    }),
+  ],
   publicDir: path.resolve(__dirname, 'assets'),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@ui': path.resolve(__dirname, './src/components/ui'),
-      '@icons': path.resolve(__dirname, './assets/icons'),
-      '@images': path.resolve(__dirname, './assets/images'),
+      '@icons': path.resolve(__dirname, './src/shared/assets/icons'),
+      '@images': path.resolve(__dirname, './src/shared/assets/images'),
       '@components': path.resolve(__dirname, './src/components'),
       '@constants': path.resolve(__dirname, './src/shared/constants'),
       '@pages': path.resolve(__dirname, './src/pages'),
